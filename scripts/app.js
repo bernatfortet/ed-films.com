16e56859658c10d16ce2ac0a1a79dc62c33d6175
Bernat <socrattes@gmail.com>
2016-05-09 12:46:52 -0700

    Updates

--- scripts/app.js
+++ scripts/app.js
@@ -9,8 +9,8 @@
     } else if (hash === '#trailer') {
       openVideoModal();
     }
-    $('body').on('click', '#closeVideoModal', closeVideoModal);
-    return $('body').on('click', '#openVideoModal', openVideoModal);
+    $('body').on('click', '.openVideoModal', openVideoModal);
+    return $('body').on('click', '.closeVideoModal', closeVideoModal);
   });
 
   showSubscribedAlert = function() {
@@ -20,13 +20,14 @@
   openVideoModal = function() {
     var youtubeId, youtubeIframe;
     youtubeId = 'S49qvE86Qs0';
-    youtubeIframe = '<iframe width="100%" height="100%" src="https://www.youtube.com/embed/' + youtubeId + '" frameborder="0" allowfullscreen></iframe>';
+    youtubeIframe = '<iframe class="videoIframe" width="100%" height="100%" src="https://www.youtube.com/embed/' + youtubeId + '" frameborder="0" allowfullscreen></iframe>';
     console.log('open');
-    $('body').append('<div id="overlayCustom" style="width:100%; height:100%; position:fixed; left:0; top:0; background-color:rgba(0,0,0,0.5);"> <a id="close" href="#" style="width:100%; display:block; margin-top:30px; color:white; text-align:center;">CLOSE</a> <div id="inner-overlay" style="position:relative; left:0; top:10px; z-index:999999;">' + youtubeIframe + '</div> </div>');
-    $('#vimeoIframe').width($('body').outerWidth() - ($('body').outerWidth() / 4));
-    $('#vimeoIframe').css('position', 'relative');
-    $('#vimeoIframe').css('left', $('body').outerWidth() / 8);
-    return $('#vimeoIframe').height($('#vimeoIframe').width() * 0.5625);
+    $('body').append('<div id="overlayCustom" class="closeVideoModal" style="width:100%; height:100%; position:fixed; left:0; top:0; background-color:rgba(0,0,0,0.5);"> <div class="inner-overlay videoIframe" style="position:relative; left:0; top:10px; z-index:999999;"> <div class="close-button closeVideoModal">×</div>' + youtubeIframe + '</div> </div>');
+    $('.videoIframe').width($('body').outerWidth() - ($('body').outerWidth() / 4));
+    $('.videoIframe').height($('.videoIframe').width() * 0.5625);
+    $('.inner-overlay').css('position', 'relative');
+    $('.inner-overlay').css('left', $('body').outerWidth() / 8);
+    return $('.inner-overlay').css('top', $('body').outerHeight() / 16);
   };
 
   closeVideoModal = function() {
