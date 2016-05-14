c71440f8b3ac8f57ec6ee11f2c8271874403a73d
Bernat <socrattes@gmail.com>
2016-05-14 14:48:14 -0700

    Updates

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,5 @@
 (function() {
-  var callPlayer, closeVideoModal, showModal, showSubscribedAlert;
+  var closeVideoModal, showModal, showSubscribedAlert;
 
   $(document).ready(function() {
     var hash;
@@ -15,27 +15,9 @@
     $('body').on('click', '.openVideoModal', function() {
       showModal('videoIframe');
       $('.videoIframe').height($('.videoIframe').width() * 0.5625);
-      return $('.videoIframe').tube();
+      return $(".videoIframe")[0].src += "&autoplay=1";
     });
-    $('body').on('click', '.closeVideoModal', closeVideoModal);
-    return $('.videoIframe').player({
-      video: 'S49qvE86Qs0',
-      width: '100%',
-      height: '100%'
-    });
-
-    /*
-    player = new (YT.Player)($('.videoIframe')[0],
-      height: '100%'
-      width: '100%'
-      videoId: 'S49qvE86Qs0'
-      events:
-        'onReady': ->
-          console.log 'onRead'
-        'onStateChange': ->
-          console.log 'on state change'
-          )
-     */
+    return $('body').on('click', '.closeVideoModal', closeVideoModal);
   });
 
   showSubscribedAlert = function() {
@@ -52,26 +34,8 @@
 
   closeVideoModal = function() {
     $('.overlay').hide();
-    return $('iframe').hide();
-  };
-
-  callPlayer = function(frame_id, func, args) {
-    var iframe;
-    if (window.jQuery && frame_id instanceof jQuery) {
-      frame_id = frame_id.get(0).id;
-    }
-    iframe = document.getElementById(frame_id);
-    if (iframe && iframe.tagName.toUpperCase() !== 'IFRAME') {
-      iframe = iframe.getElementsByTagName('iframe')[0];
-    }
-    if (iframe) {
-      iframe.contentWindow.postMessage(JSON.stringify({
-        'event': 'command',
-        'func': func,
-        'args': args || [],
-        'id': frame_id
-      }), '*');
-    }
+    $('iframe').hide();
+    return $(".videoIframe")[0].src = "https://www.youtube.com/embed/S49qvE86Qs0?enablejsapi=1";
   };
 
 }).call(this);
